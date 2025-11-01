f565e0aa53db5019c9e07f9c5fc0dc581e666b30
Carlos Matecki <cmatecki@my.centennialcollege.ca>
2025-11-01 18:16:48 -0400

    port change because 5000 is always on use

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use('/api/users', userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const MONGODB_URI = process.env.MONGODB_URI || '';
 
 async function start() {
